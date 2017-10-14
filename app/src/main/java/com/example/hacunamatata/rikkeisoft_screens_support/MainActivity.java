4f3f544fa7c135d17af8c87b9526c811099193dd
MartinDang <qwerty.lynkaa@gmail.com>
2017-10-14 09:59:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hacunamatata/rikkeisoft_screens_support/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hacunamatata.rikkeisoft_screens_support;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
